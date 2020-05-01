06893d887b949532f76d9ceb9a240394fbe02998
Niramay_Vaidya <niramay.vaidya@gmail.com>
2020-05-01 23:26:02 +0530

    Added a custom js script to detect mobile or desktop environment

---
+++ js/detectEnv.js
@@ -0,0 +1,15 @@
+
+var className;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    className = "col-sm-4 portfolio-item";
+}
+else {
+    className = "col-sm-4 portfolio-item-last";
+}
+
+document.getElementById("env-1").className = className;
+document.getElementById("env-2").className = className;
+document.getElementById("env-9").className = className;
+document.getElementById("env-10").className = className;
+document.getElementById("env-11").className = className;
